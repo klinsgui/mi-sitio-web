066ca2d23f40da48077cf3d4e8e642dcf0e21d68
Klinsmann Martinez <klinsmanm95@mesoamericana.edu.gt>
2015-10-09 10:40:33 -0600

    Css y link

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+rom django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
@@ -6,3 +7,7 @@ from .models import Postear
 def listar_publicaciones(request):
     publicaciones = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_publicaciones.html', {'publicaciones':publicaciones})
+
+def detalle_publicacion(request,pk):
+    publicacion = Poster.get_object_or_404(Postear,pk=pk)
+    return render(request,'blog/detalle_publicacion.html',{'publicacion':publicacion})
