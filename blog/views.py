99d7d452db5aa9baba2994fcbf1cff4f6705eca0
Klinsmann Martinez <klinsmanm95@mesoamericana.edu.gt>
2015-10-23 12:15:41 -0600

    Cambios varios

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def listar_publicaciones(request):
@@ -11,3 +13,29 @@ def listar_publicaciones(request):
 def detalle_publicacion(request,pk):
     publicacion = get_object_or_404(Postear,pk=pk)
     return render(request,'blog/detalle_publicacion.html',{'publicacion':publicacion})
+
+def nueva_publicacion(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicacion', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/editar_publicacion.html', {'form': form})
+
+
+def editar_publicacion(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicacion', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/editar_publicacion.html', {'form': form})
